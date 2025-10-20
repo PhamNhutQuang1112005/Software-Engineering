1774469eee89c6ae6fb221d4691d85319e33bb72
MinhLysA <lle88862@gmail.com>
2025-10-20 18:44:58 +0700

    update

--- GUI/UC_TrangChu.Designer.cs
+++ GUI/UC_TrangChu.Designer.cs
@@ -28,14 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            Guna.UI2.WinForms.Guna2HtmlLabel slogan4;
+            Guna.UI2.WinForms.Guna2HtmlLabel slogan3;
+            Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel2;
+            Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel3;
             this.gradient_box_message = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
-            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
-            this.slogan4 = new Guna.UI2.WinForms.Guna2HtmlLabel();
-            this.slogan3 = new Guna.UI2.WinForms.Guna2HtmlLabel();
-            this.guna2HtmlLabel2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
-            this.guna2HtmlLabel3 = new Guna.UI2.WinForms.Guna2HtmlLabel();
             this.logo = new Guna.UI2.WinForms.Guna2CirclePictureBox();
-            this.gradient_box_message.SuspendLayout();
+            slogan4 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            slogan3 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            guna2HtmlLabel2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            guna2HtmlLabel3 = new Guna.UI2.WinForms.Guna2HtmlLabel();
             ((System.ComponentModel.ISupportInitialize)(this.logo)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,77 +46,68 @@
             this.gradient_box_message.BackColor = System.Drawing.Color.Transparent;
             this.gradient_box_message.BorderRadius = 20;
             this.gradient_box_message.BorderThickness = 1;
-            this.gradient_box_message.Controls.Add(this.guna2HtmlLabel1);
             this.gradient_box_message.FillColor = System.Drawing.Color.Black;
             this.gradient_box_message.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
             this.gradient_box_message.FillColor3 = System.Drawing.Color.Transparent;
             this.gradient_box_message.FillColor4 = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
-            this.gradient_box_message.Location = new System.Drawing.Point(16, 242);
+            this.gradient_box_message.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.gradient_box_message.Location = new System.Drawing.Point(0, 242);
             this.gradient_box_message.Name = "gradient_box_message";
             this.gradient_box_message.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.gradient_box_message.Size = new System.Drawing.Size(884, 159);
+            this.gradient_box_message.Size = new System.Drawing.Size(925, 397);
             this.gradient_box_message.TabIndex = 20;
-            this.gradient_box_message.Paint += new System.Windows.Forms.PaintEventHandler(this.gradient_box_message_Paint);
-            // 
-            // guna2HtmlLabel1
-            // 
-            this.guna2HtmlLabel1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
-            this.guna2HtmlLabel1.ForeColor = System.Drawing.Color.White;
-            this.guna2HtmlLabel1.Location = new System.Drawing.Point(13, 18);
-            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
-            this.guna2HtmlLabel1.Size = new System.Drawing.Size(853, 18);
-            this.guna2HtmlLabel1.TabIndex = 0;
-            this.guna2HtmlLabel1.Text = "text placeholder: GreenSol ra đời với một mục tiêu rõ ràng: mang đến những giải p" +
-    "háp thông minh và bền vững cho công tác quan trắc môi trường.";
             // 
             // slogan4
             // 
-            this.slogan4.AutoSize = false;
-            this.slogan4.BackColor = System.Drawing.Color.Transparent;
-            this.slogan4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.slogan4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(220)))), ((int)(((byte)(89)))));
-            this.slogan4.Location = new System.Drawing.Point(673, 158);
-            this.slogan4.Name = "slogan4";
-            this.slogan4.Size = new System.Drawing.Size(242, 48);
-            this.slogan4.TabIndex = 19;
-            this.slogan4.Text = "thông minh!";
+            slogan4.AutoSize = false;
+            slogan4.BackColor = System.Drawing.Color.Transparent;
+            slogan4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            slogan4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(220)))), ((int)(((byte)(89)))));
+            slogan4.Location = new System.Drawing.Point(673, 158);
+            slogan4.Name = "slogan4";
+            slogan4.Size = new System.Drawing.Size(242, 48);
+            slogan4.TabIndex = 19;
+            slogan4.TabStop = false;
+            slogan4.Text = "thông minh!";
             // 
             // slogan3
             // 
-            this.slogan3.AutoSize = false;
-            this.slogan3.BackColor = System.Drawing.Color.Transparent;
-            this.slogan3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.slogan3.ForeColor = System.Drawing.Color.White;
-            this.slogan3.Location = new System.Drawing.Point(482, 158);
-            this.slogan3.Name = "slogan3";
-            this.slogan3.Size = new System.Drawing.Size(199, 48);
-            this.slogan3.TabIndex = 18;
-            this.slogan3.Text = "...Giải pháp";
+            slogan3.AutoSize = false;
+            slogan3.BackColor = System.Drawing.Color.Transparent;
+            slogan3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            slogan3.ForeColor = System.Drawing.Color.White;
+            slogan3.Location = new System.Drawing.Point(482, 158);
+            slogan3.Name = "slogan3";
+            slogan3.Size = new System.Drawing.Size(199, 48);
+            slogan3.TabIndex = 18;
+            slogan3.TabStop = false;
+            slogan3.Text = "...Giải pháp";
             // 
             // guna2HtmlLabel2
             // 
-            this.guna2HtmlLabel2.AutoSize = false;
-            this.guna2HtmlLabel2.BackColor = System.Drawing.Color.Transparent;
-            this.guna2HtmlLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.guna2HtmlLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(109)))), ((int)(((byte)(77)))));
-            this.guna2HtmlLabel2.Location = new System.Drawing.Point(457, 104);
-            this.guna2HtmlLabel2.Name = "guna2HtmlLabel2";
-            this.guna2HtmlLabel2.Size = new System.Drawing.Size(211, 48);
-            this.guna2HtmlLabel2.TabIndex = 17;
-            this.guna2HtmlLabel2.Text = "vững vàng...";
+            guna2HtmlLabel2.AutoSize = false;
+            guna2HtmlLabel2.BackColor = System.Drawing.Color.Transparent;
+            guna2HtmlLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            guna2HtmlLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(109)))), ((int)(((byte)(77)))));
+            guna2HtmlLabel2.Location = new System.Drawing.Point(457, 104);
+            guna2HtmlLabel2.Name = "guna2HtmlLabel2";
+            guna2HtmlLabel2.Size = new System.Drawing.Size(211, 48);
+            guna2HtmlLabel2.TabIndex = 17;
+            guna2HtmlLabel2.TabStop = false;
+            guna2HtmlLabel2.Text = "vững vàng...";
             // 
             // guna2HtmlLabel3
             // 
-            this.guna2HtmlLabel3.AutoSize = false;
-            this.guna2HtmlLabel3.BackColor = System.Drawing.Color.Transparent;
-            this.guna2HtmlLabel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.guna2HtmlLabel3.ForeColor = System.Drawing.Color.White;
-            this.guna2HtmlLabel3.Location = new System.Drawing.Point(340, 104);
-            this.guna2HtmlLabel3.Name = "guna2HtmlLabel3";
-            this.guna2HtmlLabel3.Size = new System.Drawing.Size(158, 48);
-            this.guna2HtmlLabel3.TabIndex = 16;
-            this.guna2HtmlLabel3.Text = "Gốc rễ";
+            guna2HtmlLabel3.AutoSize = false;
+            guna2HtmlLabel3.BackColor = System.Drawing.Color.Transparent;
+            guna2HtmlLabel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            guna2HtmlLabel3.ForeColor = System.Drawing.Color.White;
+            guna2HtmlLabel3.Location = new System.Drawing.Point(340, 104);
+            guna2HtmlLabel3.Name = "guna2HtmlLabel3";
+            guna2HtmlLabel3.Size = new System.Drawing.Size(158, 48);
+            guna2HtmlLabel3.TabIndex = 16;
+            guna2HtmlLabel3.TabStop = false;
+            guna2HtmlLabel3.Text = "Gốc rễ";
             // 
             // logo
             // 
@@ -134,17 +127,15 @@
             this.AutoScroll = true;
             this.BackColor = System.Drawing.Color.Transparent;
             this.Controls.Add(this.gradient_box_message);
-            this.Controls.Add(this.slogan4);
-            this.Controls.Add(this.slogan3);
-            this.Controls.Add(this.guna2HtmlLabel2);
-            this.Controls.Add(this.guna2HtmlLabel3);
+            this.Controls.Add(slogan4);
+            this.Controls.Add(slogan3);
+            this.Controls.Add(guna2HtmlLabel2);
+            this.Controls.Add(guna2HtmlLabel3);
             this.Controls.Add(this.logo);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "UC_TrangChu";
-            this.Size = new System.Drawing.Size(928, 430);
+            this.Size = new System.Drawing.Size(928, 642);
             this.Load += new System.EventHandler(this.UC_TrangChu_Load);
-            this.gradient_box_message.ResumeLayout(false);
-            this.gradient_box_message.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.logo)).EndInit();
             this.ResumeLayout(false);
 
@@ -153,11 +144,6 @@
         #endregion
 
         private Guna.UI2.WinForms.Guna2CustomGradientPanel gradient_box_message;
-        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
-        private Guna.UI2.WinForms.Guna2HtmlLabel slogan4;
-        private Guna.UI2.WinForms.Guna2HtmlLabel slogan3;
-        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel2;
-        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel3;
         private Guna.UI2.WinForms.Guna2CirclePictureBox logo;
     }
 }
--- GUI/UC_TrangChu.cs
+++ GUI/UC_TrangChu.cs
@@ -18,13 +18,49 @@ namespace GUI
         }
 
         private void UC_TrangChu_Load(object sender, EventArgs e)
-        {
+{
+    // Khung chứa văn bản (panel) – nhớ đúng tên control của bạn
+    gradient_box_message.Padding = new Padding(16);
+    gradient_box_message.BackColor = Color.Transparent;
+    gradient_box_message.Controls.Clear();
 
-        }
+    // Nội dung văn bản (HTML đơn giản: <b>, <br>)
+    string introHtml =
+        "<b>GreenSol</b> ra đời với một mục tiêu rõ ràng: mang đến những giải pháp thông minh và bền vững cho công tác quan trắc môi trường. " +
+        "Chúng tôi tin rằng một tương lai xanh hơn bắt đầu từ việc quản lý minh bạch, dữ liệu chính xác và những công cụ giúp doanh nghiệp " +
+        "cũng như cơ quan quản lý dễ dàng theo dõi, đánh giá chất lượng môi trường xung quanh.<br><br>" +
 
-        private void gradient_box_message_Paint(object sender, PaintEventArgs e)
-        {
+        "<b>GreenSol</b> được thành lập để giải quyết những khó khăn thường gặp trong việc quản lý hợp đồng, đơn hàng và lịch trình quan trắc. " +
+        "Thay vì phụ thuộc vào giấy tờ phức tạp hay bảng tính thủ công dễ sai sót, chúng tôi mang đến một nền tảng số hóa toàn diện – " +
+        "giúp bạn tiết kiệm thời gian, nâng cao độ tin cậy và đảm bảo tuân thủ quy định pháp luật.<br><br>" +
 
-        }
+        "Sứ mệnh của <b>GreenSol</b> là xây dựng một hệ sinh thái quản lý hiện đại, minh bạch và thân thiện, góp phần hỗ trợ doanh nghiệp " +
+        "hoạt động hiệu quả hơn, đồng thời hướng tới một hành tinh sạch và bền vững. Trong tương lai, <b>GreenSol</b> sẽ không ngừng mở rộng, " +
+        "tích hợp thêm nhiều tính năng thông minh, để trở thành người bạn đồng hành tin cậy của bạn trên hành trình bảo vệ môi trường.<br><br>" +
+
+        "Khi bắt đầu sử dụng <b>GreenSol</b>, bạn không chỉ đang quản lý công việc hiệu quả hơn, mà còn cùng chúng tôi đặt một viên gạch vững chắc " +
+        "cho tương lai xanh – nơi dữ liệu và công nghệ phục vụ cho sự phát triển bền vững.";
+
+    // Tạo label HTML và chèn nội dung
+    var html = new Guna.UI2.WinForms.Guna2HtmlLabel
+    {
+        Name = "lblIntro",
+        BackColor = Color.Transparent,
+        AutoSize = false,
+        Dock = DockStyle.Fill,
+        Padding = new Padding(2, 0, 2, 4),
+        // Nếu muốn cỡ chữ lớn hơn, tăng size dưới đây
+        Font = new Font("Segoe UI", 10f),
+        Text = introHtml,
+        ForeColor = Color.White
+    };
+
+    gradient_box_message.Controls.Add(html);
+
+    // Khi thay đổi kích thước, vẽ lại khung gradient cho nét
+    gradient_box_message.Resize += (s, _e) => gradient_box_message.Invalidate();
+}
+
+
     }
 }
