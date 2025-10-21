b07bc9874fa03fe8e8480d8fa41f1eab8c48387d
MinhLysA <lle88862@gmail.com>
2025-10-21 14:12:46 +0700

    update

---
+++ DAL/DBConnection.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class DBConnection
+    {
+        private static readonly string configPath = "config.txt";
+
+        public static string GetConnectionString()
+        {
+            if (!File.Exists(configPath))
+                throw new FileNotFoundException("Không tìm thấy file cấu hình kết nối!");
+
+            string[] lines = File.ReadAllLines(configPath);
+            if (lines.Length < 3)
+                throw new Exception("File cấu hình không hợp lệ!");
+
+            string authen = lines[0].Trim().ToLower();
+            string server = lines[1].Trim();
+            string db = lines[2].Trim();
+
+            if (authen == "windows")
+            {
+                return $"Data Source={server};Initial Catalog={db};Integrated Security=True;TrustServerCertificate=True;";
+            }
+            else if (authen == "sql" && lines.Length >= 5)
+            {
+                string uid = lines[3].Trim();
+                string pw = lines[4].Trim();
+                return $"Data Source={server};Initial Catalog={db};User ID={uid};Password={pw};TrustServerCertificate=True;";
+            }
+
+            throw new Exception("Cấu hình không hợp lệ hoặc thiếu thông tin.");
+        }
+
+        public static SqlConnection GetConnection()
+        {
+            string connStr = GetConnectionString();
+            return new SqlConnection(connStr);
+        }
+    }
+}
---
+++ GUI/GUI_Form_Config.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class GUI_Form_Config : Form
+    {
+        public GUI_Form_Config()
+        {
+            InitializeComponent();
+        }
+
+        private void frmConfig_Load(object sender, EventArgs e)
+        {
+            if (ckW.Checked == true)
+            {
+                txtuid.ReadOnly = true;
+                txtPass.ReadOnly = true;
+            }
+            else
+            {
+                txtuid.ReadOnly = false;
+                txtPass.ReadOnly = false;
+            }
+        }
+
+        private void bSave_Click_1(object sender, EventArgs e)
+        {
+            StreamWriter sw = new StreamWriter("config.txt");
+            if (ckW.Checked == true)
+            {
+                sw.WriteLine("windows");
+                sw.WriteLine(txtserver.Text);
+                sw.WriteLine(txtdb.Text);
+
+
+            }
+            else
+            {
+                sw.WriteLine("sql");
+                sw.WriteLine(txtserver.Text);
+                sw.WriteLine(txtdb.Text);
+                sw.WriteLine(txtuid.Text);
+                sw.WriteLine(txtPass.Text);
+            }
+            sw.Close();
+            MessageBox.Show("Lưu thành công");
+            this.Close();
+        }
+
+        private void ckW_CheckedChanged_1(object sender, EventArgs e)
+        {
+            if (ckW.Checked == true)
+            {
+                txtuid.ReadOnly = true;
+                txtPass.ReadOnly = true;
+            }
+            else
+            {
+                txtuid.ReadOnly = false;
+                txtPass.ReadOnly = false;
+            }
+        }
+
+        private void bCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
