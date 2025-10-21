32538988f308e545c69536cd028b71479a281b6d
MinhLysA <lle88862@gmail.com>
2025-10-21 19:48:36 +0700

    Sửa Giao diện

--- WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b07bc9874fa03fe8e8480d8fa41f1eab8c48387d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b7c0b5fb3ab5acd23b8e98730530dbde90cf1db")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
